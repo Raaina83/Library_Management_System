255efd7e215c92588f7e369461ea8d582fbaf922
Raaina <raainawhalla83@gmail.com>
2025-02-05 17:12:22 +0530

    "CORS error solved"

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ cloudinary.config({
 app.use(express.json()) //to parse the incoming request with JSON payloads from req.body
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:8080", "https://library-management-system-lvih.onrender.com"], 
+    origin: ["http://localhost:5173", "http://localhost:8080", "https://lms-1k22.onrender.com"], 
     credentials: true
 }))
 
